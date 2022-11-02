a5bab8b9b2e59bdf5e5fe78bf03794c3c6732a4f
BBlack-Hun <h1103j@kangwon.ac.kr>
2022-11-02 18:26:22 +0900

    Initial commit

---
+++ src/test/java/com/example/ooppractice2/GradeCalculatorTest.java
@@ -0,0 +1,2 @@
+package com.example.ooppractice2;public class GradeCalculatorTest {
+}
---
+++ src/test/java/com/example/ooppractice2/OopPractice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ooppractice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopPractice2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
